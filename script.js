ed2655f38201a846fa82d464ba606a3de8fd2b53
FaVaKu <83724583+FaVaKu@users.noreply.github.com>
2024-09-27 13:48:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const token = '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';
+const token = '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';
 const repo = 'FaVaKu/Project_Manager';
 const file = 'data.json';
 
@@ -9,21 +9,33 @@ form.addEventListener('submit', (e) => {
   const formData = new FormData(form);
   const headers = {
     'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.github.v3+json'
   };
   const data = {
     message: 'Обновление файла',
-    content: JSON.stringify({
+    content: btoa(JSON.stringify({
       name: formData.get('name'),
       email: formData.get('email')
-    })
+    })),
+    sha: ''
   };
   fetch(`https://api.github.com/repos/${repo}/contents/${file}`, {
-    method: 'PUT',
-    headers,
-    body: JSON.stringify(data)
+    method: 'GET',
+    headers
   })
+  .then(response => response.json())
+  .then(data => {
+    const sha = data.sha;
+    data.sha = sha;
+    fetch(`https://api.github.com/repos/${repo}/contents/${file}`, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(data)
+    })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
 });
